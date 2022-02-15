b79b29c0edd63e54f344f7be603d195bdd970d57
Akaki <98376156+shkippppper@users.noreply.github.com>
2022-02-15 08:59:30 +0400

    Add files via upload

---
+++ RegexValidatePINCode.py
@@ -0,0 +1,14 @@
+
+
+
+def validate_pin(pin):
+
+    if pin.isnumeric():
+        if len(pin)==4 or len(pin) == 6:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
